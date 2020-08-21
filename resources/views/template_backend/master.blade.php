99c60ee9ec41a82e205c5e350bb4a76a240c89d8
SendraIlhamMandala <1706062@sttgarut.ac.id>
2020-08-21 22:48:51 +0700

    menambah custom css

--- resources/views/template_backend/master.blade.php
+++ resources/views/template_backend/master.blade.php
@@ -12,8 +12,10 @@
 
   <!-- CSS Libraries -->
   <script src="js/app.js"></script>
+  
 
   <!-- Template CSS -->
+  <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/css/components.css') }}">
 <!-- Start GA -->
@@ -43,7 +45,7 @@
           <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
             <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
             <div class="d-sm-none d-lg-inline-block">Hi, {{ Auth::user()->name }}</div></a>
-            <div class="dropdown-menu dropdown-menu-right">
+            <div class="dropdown-menu dropdown-menu-right" >
                 <a href="features-profile.html" class="dropdown-item has-icon">
                     <i class="far fa-user"></i> Profile
                 </a>

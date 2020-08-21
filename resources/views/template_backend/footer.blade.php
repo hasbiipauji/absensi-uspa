ea8da4ab7ff7d5337f5e9d4b3d639823191c0cf1
SendraIlhamMandala <1706062@sttgarut.ac.id>
2020-08-21 17:28:18 +0700

    commit for heroku

--- resources/views/template_backend/footer.blade.php
+++ resources/views/template_backend/footer.blade.php
@@ -8,13 +8,14 @@
 </div>
 
 <!-- General JS Scripts -->
-<script src="{{ asset('assets/modules/jquery.min.js') }}"></script>
+{{-- <script src="{{ asset('assets/modules/jquery.min.js') }}"></script>
 <script src="{{ asset('assets/modules/popper.js') }}"></script>
 <script src="{{ asset('assets/modules/tooltip.js') }}"></script>
 <script src="{{ asset('assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
 <script src="{{ asset('assets/modules/moment.min.js') }}"></script>
-<script src="{{ asset('assets/js/stisla.js') }}"></script>
+<script src="{{ asset('assets/js/stisla.js') }}"></script> --}}
+
+<script src="{{ asset('assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
 
 <!-- JS Libraies -->
 
--- resources/views/template_backend/master.blade.php
+++ resources/views/template_backend/master.blade.php
@@ -7,10 +7,12 @@
   <title>Absen &mdash; USPA MEDIA NUSANTARA</title>
 
   <!-- General CSS Files -->
+  
   <link rel="stylesheet" href="{{ asset('assets/modules/bootstrap/css/bootstrap.min.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/modules/fontawesome/css/all.min.css') }}">
 
   <!-- CSS Libraries -->
+  
 
   <!-- Template CSS -->
   <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
@@ -102,3 +104,4 @@
       @include('template_backend.footer')
 
 
+      

ccabd4fd2a161e74689806250c947499d3be932a
hasbiipauji <hasbiipauji@gmail.com>
2020-08-22 13:30:14 +0700

    nambah user di sidebar

--- resources/views/template_backend/sidebar.blade.php
+++ resources/views/template_backend/sidebar.blade.php
@@ -39,6 +39,13 @@
               <li><a class="nav-link" href="">List User</a></li>
             </ul>
          </li>
+
+         <li class="dropdown">
+            <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-user"></i> <span>User</span></a>
+            <ul class="dropdown-menu">
+              <li><a class="nav-link" href="">List User</a></li>
+            </ul>
+         </li>
       </aside>
    </div>
 

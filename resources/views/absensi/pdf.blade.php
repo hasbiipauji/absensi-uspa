d9f73a39608684345f58ad9481e59ff15e593ce1
hasbiipauji <hasbiipauji@gmail.com>
2020-08-30 21:29:53 +0700

    perubahan sedikit di pdf.blade

--- resources/views/absensi/pdf.blade.php
+++ resources/views/absensi/pdf.blade.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
   </head>
   <body>
-    <div style="overflow-x: auto">
+    <div>
         <table class="table table-striped table-hover table-sm table-bordered" style="width: 100% ; max-width:100%; white-space:nowrap;"  >
         <thead class="thead-dark">
             <tr>
